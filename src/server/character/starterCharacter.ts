ec1f89cf58f75809dea1a35b92eefc3ca925104a
abod <you@example.com>
2025-06-21 16:17:09 +0300

    refactor: change lifecycle method from OnInit to OnStart in StarterCharacter

--- src/server/character/starterCharacter.ts
+++ src/server/character/starterCharacter.ts
@@ -1,9 +1,9 @@
-import { OnInit, Service } from "@flamework/core";
+import { OnStart, Service } from "@flamework/core";
 import { ReplicatedStorage, RunService, StarterPlayer } from "@rbxts/services";
 
 @Service({})
-export class StarterCharacter implements OnInit {
-	onInit() {
+export class StarterCharacter implements OnStart {
+	onStart() {
         if (RunService.IsStudio()) {
             const StarterCharacter = ReplicatedStorage.Assets.StarterCharacter;
             StarterCharacter.Parent = StarterPlayer
